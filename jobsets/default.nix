61b5c012f655d2981483670ea062af71b6f871d4
Drew Hess <src@drewhess.com>
2019-05-16 10:12:09 +0100

    Add Hydra jobs.

---
+++ jobsets/default.nix
@@ -0,0 +1,74 @@
+# Based on
+# https://github.com/input-output-hk/iohk-ops/blob/df01a228e559e9a504e2d8c0d18766794d34edea/jobsets/default.nix
+
+{ nixpkgs ? <nixpkgs>
+, declInput ? {}
+}:
+
+let
+
+  nixosYubiKeyUri = "https://github.com/dhess/nixos-yubikey.git";
+
+  mkFetchGithub = value: {
+    inherit value;
+    type = "git";
+    emailresponsible = false;
+  };
+
+  pkgs = import nixpkgs {};
+
+  defaultSettings = {
+    enabled = 1;
+    hidden = false;
+    keepnr = 20;
+    schedulingshares = 100;
+    checkinterval = 60;
+    enableemail = false;
+    emailoverride = "";
+    nixexprpath = "jobsets/release.nix";
+    nixexprinput = "nixosYubiKey";
+    description = "nixos-yubikey";
+    inputs = {
+      nixosYubiKey = mkFetchGithub "${nixosYubiKeyUri} master";
+    };
+  };
+
+  # Build against a nixpkgs-channels repo. This can run fairly often
+  # as the channels don't update so much.
+  mkNixpkgsChannels = nixosYubiKeyBranch: nixpkgsRev: {
+    inputs = {
+      nixosYubiKey = mkFetchGithub "${nixosYubiKeyUri} ${nixosYubiKeyBranch}";
+      nixpkgs_override = mkFetchGithub "https://github.com/NixOS/nixpkgs-channels.git ${nixpkgsRev}";
+      checkinterval = 60 * 60;
+    };
+  };
+
+  # Build against the nixpkgs repo. Runs less often due to nixpkgs'
+  # velocity.
+  mkNixpkgs = nixosYubiKeyBranch: nixpkgsRev: {
+    checkinterval = 60 * 60 * 12;
+    inputs = {
+      nixosYubiKey = mkFetchGithub "${nixosYubiKeyUri} ${nixosYubiKeyBranch}";
+      nixpkgs_override = mkFetchGithub "https://github.com/NixOS/nixpkgs.git ${nixpkgsRev}";
+    };
+  };
+
+  mainJobsets = with pkgs.lib; mapAttrs (name: settings: defaultSettings // settings) (rec {
+    master = {};
+    nixos-unstable = mkNixpkgsChannels "master" "nixos-unstable";
+    nixpkgs-unstable = mkNixpkgsChannels "master" "nixpkgs-unstable";
+    nixpkgs = mkNixpkgs "master" "master";
+  });
+
+  jobsetsAttrs = mainJobsets;
+
+  jobsetJson = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
+
+in {
+  jobsets = with pkgs.lib; pkgs.runCommand "spec.json" {} ''
+    cat <<EOF
+    ${builtins.toJSON declInput}
+    EOF
+    cp ${jobsetJson} $out
+  '';
+}
---
+++ jobsets/release.nix
@@ -0,0 +1,43 @@
+let
+
+  lib = import ../lib;
+  inherit (lib) fixedNixpkgs;
+  nixos-yubikey = (import ../.) {};
+
+in
+
+{ supportedSystems ? [ "x86_64-linux" ]
+, scrubJobs ? true
+, nixpkgsArgs ? {
+    config = { inHydra = true; };
+  }
+}:
+
+with import (fixedNixpkgs + "/pkgs/top-level/release-lib.nix") {
+  inherit supportedSystems scrubJobs nixpkgsArgs;
+};
+
+let
+
+  x86_64 = [ "x86_64-linux" "x86_64-darwin" ];
+  x86_64_linux = [ "x86_64-linux" ];
+  linux = [ "x86_64-linux" "aarch64-linux" ];
+
+  jobs = (mapTestOn (rec {
+
+    nixos-yubikey = x86_64_linux;
+
+  })) // (rec {
+
+    x86_64-linux = pkgs.releaseTools.aggregate {
+      name = "nixos-yubikey-x86_64-linux";
+      meta.description = "nixos-yubikey (x86_64-linux)";
+      constituents = with jobs; [
+        nixos-yubikey.x86_64-linux
+      ];
+    };
+
+  });
+
+in
+jobs
